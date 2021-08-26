0cf8b955589a2b1a4205b474c3342b08551d30c9
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-26 14:40:39 -0300

    Exercicio 02

---
+++ src/br/com/zup/ListaDeExerciciosEstruturaDeDecisao02.java
@@ -0,0 +1,17 @@
+package br.com.zup;
+
+import java.util.Scanner;
+
+public class ListaDeExerciciosEstruturaDeDecisao02 {
+    public static void main(String[] args) {
+        // 2 - Faça um programa que receba o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+        Scanner leitorValores = new Scanner(System.in);
+        float produto1;
+        float produto2;
+
+
+
+
+
+    }
+}
