9c0f2288e500d57188d230102c71a8c79e358fcf
Arê Nascimento <aretuza.gab@gmail.com>
2021-08-26 15:24:27 -0300

    resolução do exercicio dias da semana switch

--- src/br/com/zup/ListaDeExerciciosEstruturaDeDecisao02.java
+++ src/br/com/zup/ListaDeExerciciosEstruturaDeDecisao02.java
@@ -5,10 +5,22 @@ import java.util.Scanner;
 public class ListaDeExerciciosEstruturaDeDecisao02 {
     public static void main(String[] args) {
         // 2 - Faça um programa que receba o preço de três produtos e informe qual produto você deve comprar, sabendo que a decisão é sempre pelo mais barato.
+
+        //inicie o Scanner
         Scanner leitorValores = new Scanner(System.in);
+
+        //Defina as variáveis
         float produto1;
         float produto2;
+        float produto3;
 
+        //Usuario informando os dados
+        System.out.println("Insira aqui o valor do produto 1");
+        produto1 = leitorValores.nextFloat();
+        System.out.println("Insira aqui o valor do produto 2");
+        produto2 = leitorValores.nextFloat();
+        System.out.println("Insira aqui o valor do produto 3");
+        produto3 = leitorValores.nextFloat();
 
 
 
